b41bd69109c0cd532dc052e469dad97ffbf7ce57
AvivitCohen99 <avivit.cohen99@gmail.com>
2023-08-16 22:34:04 +0300

    reading xml - continue

--- engine/src/definition/property/impl/IntegerPropertyDefinition.java
+++ engine/src/definition/property/impl/IntegerPropertyDefinition.java
@@ -3,8 +3,31 @@ package definition.property.impl;
 import definition.property.api.AbstractPropertyDefinition;
 import definition.property.api.PropertyType;
 import definition.value.generator.api.ValueGenerator;
+import definition.value.generator.fixed.FixedValueGenerator;
+import definition.value.generator.random.impl.numeric.RandomIntegerGenerator;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+
 
 public class IntegerPropertyDefinition extends AbstractPropertyDefinition<Integer> {
+
+    public static IntegerPropertyDefinition parse(Element propertyElement, String name) {
+        Element propValueElement = (Element) propertyElement.getElementsByTagName("PRD-value").item(0);
+
+        ValueGenerator generator;
+
+        Boolean isRandom = propValueElement != null ? Boolean.parseBoolean(propValueElement.getAttribute("random-initialize")) : true;
+        if (!isRandom) {
+            Integer init = Integer.parseInt(propValueElement.getAttribute("init"));
+            generator = new FixedValueGenerator(init);
+        } else {
+            NamedNodeMap range = propertyElement.getElementsByTagName("PRD-range").item(0).getAttributes();
+            Integer from = Integer.parseInt(range.getNamedItem("from").getNodeValue());
+            Integer to = Integer.parseInt(range.getNamedItem("to").getNodeValue());
+            generator = new RandomIntegerGenerator(from, to);
+        }
+        return new IntegerPropertyDefinition(name, generator);
+    }
 
     public IntegerPropertyDefinition(String name, ValueGenerator<Integer> valueGenerator) {
         super(name, PropertyType.DECIMAL, valueGenerator);
